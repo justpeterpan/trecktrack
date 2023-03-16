7460ddccca94eabb245db7cd8602cfc0e4d62968
justpeterpan <peter.busch@posteo.net>
2023-03-16 18:03:53 +0100

    fix: wrap Dialog into ClientOnly to prevent hydration errors

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -44,48 +44,45 @@ const mobileMenuOpen = ref(false)
         <SunIcon class="h-6 w-6 text-white" aria-hidden="true" />
       </div>
     </nav>
-    <Dialog
-      as="div"
-      class="lg:hidden"
-      @close="mobileMenuOpen = false"
-      :open="mobileMenuOpen"
-    >
-      <div class="fixed inset-0 z-10" />
-      <DialogPanel
-        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
+    <ClientOnly>
+      <Dialog
+        as="div"
+        class="lg:hidden"
+        @close="mobileMenuOpen = false"
+        :open="mobileMenuOpen"
       >
-        <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-            <img
-              class="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt=""
-            />
-          </a>
-          <button
-            type="button"
-            class="-m-2.5 rounded-md p-2.5 text-gray-400"
-            @click="mobileMenuOpen = false"
-          >
-            <span class="sr-only">Close menu</span>
-            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/25">
-            <div class="space-y-2 py-6">
-              <a
-                v-for="item in navigation"
-                :key="item.name"
-                :href="item.path"
-                class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                >{{ item.name }}</a
-              >
+        <div class="fixed inset-0 z-10" />
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
+        >
+          <div class="flex items-center justify-between">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">TrekTrack</span>
+            </a>
+            <button
+              type="button"
+              class="-m-2.5 rounded-md p-2.5 text-gray-400"
+              @click="mobileMenuOpen = false"
+            >
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/25">
+              <div class="space-y-2 py-6">
+                <a
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :href="item.path"
+                  class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                  >{{ item.name }}</a
+                >
+              </div>
             </div>
           </div>
-        </div>
-      </DialogPanel>
-    </Dialog>
+        </DialogPanel>
+      </Dialog>
+    </ClientOnly>
   </header>
 </template>
